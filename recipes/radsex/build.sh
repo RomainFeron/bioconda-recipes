483f1c0abc935d22f0925d332c8419835676e7bd
RomainFeron <romain.feron@evobio.eu>
2018-10-29 22:21:23 +0100

    Fixes for zlib

--- recipes/radsex/build.sh
+++ recipes/radsex/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH=$PREFIX/include
-export LIBRARY_PATH=$PREFIX/lib
+# export CFLAGS="$PREFIX/include"
+# export LDFLAGS="$PREFIX/lib"
+# export CPATH=${PREFIX}/include
+
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
 make
